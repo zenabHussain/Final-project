a5b2ae24331ce1a63d87f0989f2968de900ed1b2
Val <valery.art@gmail.com>
2023-06-01 05:20:37 +0300

    updates

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
 
 function validateForm() {
-    // Clear previous error messages
-    clearErrors();
-    
-    // Get form field values
-    var taskName = document.getElementById("taskName").value;
-    var description = document.getElementById("description").value;
-    var assignedTo = document.getElementById("assignedTo").value;
-    var dueDate = document.getElementById("dueDate").value;
-    var status = document.getElementById("status").value;
-    
-    // Flag to track form validity
-    var isValid = true;
-
-    // Validate Task Name
-    if (taskName === "") {
-      alert("taskNameError", "Task Name is required.");
-      isValid = false;
-    }
-
-    // Validate Description
-    if (description=== "") {
-      alert("descriptionError", "Description is required.");
-      isValid = false;
-    }
-
-    // Validate Assigned To
-    if (assignedTo === "") {
-      alert("assignedToError", "Assigned To is required.");
-      isValid = false;
-    }
-
-    // Validate Due Date
-    if (dueDate === "") {
-      alert("dueDateError", "Due Date is required.");
-      isValid = false;
-    }
-
-    // Validate Status
-    if (status === "") {
-      displayError("statusError", "Status is required.");
-      isValid = false;
-    }
-
-    return isValid;
-  }
-
-  function displayError(id, message) {
-    var errorElement = document.getElementById(id);
-    errorElement.innerText = message;
-  }
-
-  function clearErrors() {
-    var errorElements = document.getElementsByClassName("error-text");
-    for (var i = 0; i < errorElements.length; i++) {
-      errorElements[i].innerText = "";
-    }
-  }
-
-  document.getElementById("task").addEventListener('click', validateForm);
+  // Clear previous error messages
+  clearErrors();
   
+  // Get form field values
+  var taskName = document.getElementById("taskName").value;
+  var description = document.getElementById("description").value;
+  var assignedTo = document.getElementById("assignedTo").value;
+  var dueDate = document.getElementById("dueDate").value;
+  var status = document.getElementById("status").value;
+  
+  // Flag to track form validity
+  var isValid = true;
+
+  // Validate Task Name
+  if (taskName === "") {
+    alert("taskNameError", "Task Name is required.");
+    isValid = false;
+  }
+
+  // Validate Description
+  if (description=== "") {
+    alert("descriptionError", "Description is required.");
+    isValid = false;
+  }
+
+  // Validate Assigned To
+  if (assignedTo === "") {
+    alert("assignedToError", "Assigned To is required.");
+    isValid = false;
+  }
+
+  // Validate Due Date
+  if (dueDate === "") {
+    displayError("dueDateError", "Due Date is required.");
+    isValid = false;
+  }
+
+  // Validate Status
+  if (status === "") {
+    displayError("statusError", "Status is required.");
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+function displayError(id, message) {
+  var errorElement = document.getElementById(id);
+  errorElement.innerText = message;
+}
+
+function clearErrors() {
+  var errorElements = document.getElementsByClassName("error-text");
+  for (var i = 0; i < errorElements.length; i++) {
+    errorElements[i].innerText = "";
+  }
+}
+
+document.getElementById("task").addEventListener('click', validateForm);
