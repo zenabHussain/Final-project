0673572844b8ac300873240efb1cafa44fa29015
Val <valery.art@gmail.com>
2023-06-05 06:58:04 +0300

    delete validation form

--- index.js
+++ index.js
@@ -1,5 +1,107 @@
-function validateForm() {
-  // Clear previous error messages
+// Task class
+class Task {
+  constructor(taskID, taskName, description, assignedTo, dueDate, status) {
+    this.taskID = taskID;
+    this.taskName = taskName;
+    this.description = description;
+    this.assignedTo = assignedTo;
+    this.dueDate = dueDate;
+    this.status = status;
+  }
+}
+
+// TaskManager class
+class TaskManager {
+  constructor() {
+    this.taskList = [];
+    this.counter = 1;
+  }
+
+  addTask(task) {
+    this.taskList.push(task);
+    this.counter++;
+  }
+
+  deleteTask(taskID) {
+    const index = this.taskList.findIndex(task => task.taskID === taskID);
+    if (index !== -1) {
+      this.taskList.splice(index, 1);
+    }
+  }
+}
+
+const taskManager = new TaskManager();
+
+// Load tasks from localStorage
+function loadTasks() {
+  const data = JSON.parse(localStorage.getItem("data")) || [];
+  for (const taskData of data) {
+    const task = new Task(
+      taskData.taskID,
+      taskData.taskName,
+      taskData.description,
+      taskData.assignedTo,
+      taskData.dueDate,
+      taskData.status
+    );
+    taskManager.addTask(task);
+    addTaskToDOM(task);
+  }
+}
+
+function saveTasks() {
+  const data = JSON.stringify(taskManager.taskList);
+  localStorage.setItem("data", data);
+}
+
+function addTaskToDOM(task) {
+  // Create task card container
+  let taskCard = document.createElement("div");
+  taskCard.className = "task-card";
+
+  // Set task card HTML
+  taskCard.innerHTML =
+    '<span class="task-id">' +
+    "</span>" +
+    "<br>" +
+    "<strong>Task Name:</strong> " +
+    task.taskName +
+    '<div class="task-details">' +
+    "<strong>Description:</strong> " +
+    task.description +
+    "<br>" +
+    "<strong>Assigned To:</strong> " +
+    task.assignedTo +
+    "<br>" +
+    "<strong>Due Date:</strong> " +
+    task.dueDate +
+    "<br>" +
+    "<strong>Status:</strong> " +
+    task.status +
+    "</div>" +
+    '<button class="delete-button" data-id="' +
+    task.taskID +
+    '">Delete</button>';
+
+  // Append task card to task list container
+  let taskListContainer = document.getElementById("taskList");
+  taskListContainer.appendChild(taskCard);
+}
+
+function deleteTaskFromDOM(taskID) {
+  let taskCards = document.getElementsByClassName("task-card");
+  for (let i = 0; i < taskCards.length; i++) {
+    let card = taskCards[i];
+    if (card.querySelector(".delete-button").getAttribute("data-id") == taskID) {
+      card.remove();
+      break;
+    }
+  }
+}
+
+function validateForm(event) {
+  event.preventDefault(); // Prevent the form from being submitted
+
   clearErrors();
 
   var taskNameInput = document.getElementById("taskName");
@@ -61,24 +163,27 @@ function validateForm() {
 
     // Reset the form
     document.getElementById("taskForm").reset();
+
+    // Save tasks to localStorage
+    saveTasks();
   }
 }
 
-// Delete task 
-
+// Delete task
 function handleDeleteTask(event) {
   if (event.target.classList.contains("delete-button")) {
     const taskID = parseInt(event.target.getAttribute("data-id"));
     taskManager.deleteTask(taskID);
     deleteTaskFromDOM(taskID);
+
+    // Save tasks to localStorage
+    saveTasks();
   }
 }
 
 document.getElementById("taskList").addEventListener("click", handleDeleteTask);
 
 // Error code
-
-
 function showError(inputElement, errorMessage) {
   inputElement.classList.add("is-invalid");
 
@@ -104,5 +209,5 @@ function clearErrors() {
 
 document.getElementById("taskForm").addEventListener("submit", validateForm);
 
-
-  document.getElementById("task").addEventListener('click', validateForm);
+// Load tasks on page load
+document.addEventListener("DOMContentLoaded", loadTasks);
